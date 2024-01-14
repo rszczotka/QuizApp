eb7a2ec79393d98629a647f0c73bcde77531bc1d
Matetorture <przemek.pytlarz18@gmail.com>
2024-01-14 15:24:01 +0100

    Smoother counter animation

--- QuizApp/src/js/question.js
+++ QuizApp/src/js/question.js
@@ -8,17 +8,19 @@ const timeDiv = document.querySelector('#time');
 const timeCircle = document.querySelector('#time-circle');
 const initTime = 30;
 let time = initTime;
-
+const howMuchCountdown = 100;
 
 timeDiv.innerHTML = time;
 const countdownTime = setInterval(() => {
-    timeDiv.innerHTML = --time;
+    time-=1/howMuchCountdown
+    time=Math.floor(time*howMuchCountdown)/howMuchCountdown
+    timeDiv.innerHTML = Math.floor(time+1);
     timeCircle.style.strokeDashoffset = 440-250*time/initTime;
-    if(time == 0){
+    if(time < 0){
         clearInterval(countdownTime);
         sendAnswer();
     }
-}, 1000);
+}, 10);
 
 answers.forEach((e, i) => {
     e.addEventListener('click', () => {
