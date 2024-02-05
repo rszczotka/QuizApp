30fd9b7734654ef8b4f626f923fff439ff3d16af
Filip Madzia <filipmadzia33@gmail.com>
2024-02-05 14:51:54 +0100

    init2

---
+++ quiz-app-api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quiz_app_api.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
