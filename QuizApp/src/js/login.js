283d94f79a398ca5a6db7487f221c8dd7afb3cb5
MP <106026306+Mpraszk@users.noreply.github.com>
2024-03-15 18:08:29 +0100

    add login js script
    added a login.js file that connects to the API and creates a cookie

---
+++ QuizApp/src/js/login.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+  document.getElementById("form").onsubmit = function () {
+    return false;
+  };
+};
+function sendApiRequest() {
+  var login = document.getElementById("login").value;
+  var password = document.getElementById("password").value;
+  if (login != undefined && login != null && login != "" && password != undefined && password != null && password) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append("Origin", "*");
+    myHeaders.append("credentials", "include");
+
+    const data = JSON.stringify({
+      login: login,
+      password: password,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: data,
+      redirect: "follow",
+    };
+    //api url
+    fetch("http://localhost:5000/api/users/Login/", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        var api_key = JSON.parse(result).api_key;
+        if (api_key != undefined) {
+          document.cookie = "api_key=" + api_key + ";";
+          document.getElementById("messageBox").innerText = "udało się zalogować";
+        } else {
+          document.getElementById("messageBox").innerText = "logowanie nieudane";
+        }
+      })
+      .catch((error) => console.error(error));
+  }
+  else{
+    document.getElementById("messageBox").innerText = "uzupełnij wszystkie pola";
+  }
+}
