f8aa41f2fd728274305346f7d813bf888b9ba38a
Filip Madzia <filipmadzia33@gmail.com>
2024-03-27 21:09:45 +0100

    Started implementing UserAnswers Controller

---
+++ quiz_app_api/Controllers/UserAnswersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using quiz_app_api.Data;
+using quiz_app_api.Data.JsonModels.UserAnswers.Input;
+
+namespace quiz_app_api.Controllers;
+
+[Route("api/UserAnswers")]
+[ApiController]
+public class UserAnswersController(AppDbContext _context) : Controller
+{
+	// POST: api/useranswers/CreateUserAnswer
+	[HttpPost]
+	[Route("CreateUserAnswer")]
+	public Task<ActionResult> CreateUserAnswer([FromBody] CreateUserAnswerJson data)
+	{
+		throw new NotImplementedException();
+	}
+
+	// GET: api/useranswers/GetUserAnswers
+	[HttpGet]
+	[Route("GetUserAnswers")]
+	public Task<ActionResult> GetUserAnswers([FromBody] GetUserAnswersJson data)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ quiz_app_api/Data/JsonModels/UserAnswers/Input/CreateUserAnswerJson.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace quiz_app_api.Data.JsonModels.UserAnswers.Input;
+
+public class CreateUserAnswerJson
+{
+	[JsonPropertyName("question_id")]
+	public int QuestionId { get; set; }
+	[JsonPropertyName("chosen_option")]
+	public int ChosenOption { get; set; }
+	[JsonPropertyName("api_key")]
+	public string ApiKey { get; set; }
+}
