1097f61808be867433d0f20bb054aa5119985c36
Filip Madzia <filipmadzia33@gmail.com>
2024-04-07 22:11:57 +0200

    Finished UserAnswers

--- quiz_app_api/Controllers/UserAnswersController.cs
+++ quiz_app_api/Controllers/UserAnswersController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using quiz_app_api.Data;
+using quiz_app_api.Data.Entities;
+using quiz_app_api.Data.JsonModels;
 using quiz_app_api.Data.JsonModels.UserAnswers.Input;
+using quiz_app_api.Data.JsonModels.UserAnswers.Output;
+using quiz_app_api.Misc;
 
 namespace quiz_app_api.Controllers;
 
@@ -11,16 +16,53 @@ public class UserAnswersController(AppDbContext _context) : Controller
 	// POST: api/useranswers/CreateUserAnswer
 	[HttpPost]
 	[Route("CreateUserAnswer")]
-	public Task<ActionResult> CreateUserAnswer([FromBody] CreateUserAnswerJson data)
+	public async Task<ActionResult> CreateUserAnswer([FromBody] CreateUserAnswerJson data)
 	{
-		throw new NotImplementedException();
+		var status = new SuccessJson();
+
+		if(data.ApiKey == null)
+		{
+			return Json(status);
+		}
+
+		try
+		{
+			var userAnswerEntity = new UserAnswerEntity
+			{
+				User = _context.UserEntities.Where(n => n.Login.Equals(APIKeyGenerator.GetLoginByAPIKey(data.ApiKey))).First(),
+				Question = _context.QuestionEntities.Where(x => x.Id == data.QuestionId).First(),
+				ChosenOption = data.ChosenOption,
+				EndTime = DateTime.Now
+			};
+
+			_context.UserAnswerEntities.Add(userAnswerEntity);
+			await _context.SaveChangesAsync();
+		}
+		catch(Exception e)
+		{
+			await Console.Out.WriteLineAsync(e.Message);
+			status.Success = false;
+			return Json(status);
+		}
+
+		status.Success = true;
+		return Json(status);
 	}
 
 	// GET: api/useranswers/GetUserAnswers
 	[HttpGet]
 	[Route("GetUserAnswers")]
-	public Task<ActionResult> GetUserAnswers([FromBody] GetUserAnswersJson data)
+	public async Task<ActionResult> GetUserAnswers([FromBody] GetUserAnswersJson data)
 	{
-		throw new NotImplementedException();
+		if(data.ApiKey == null || !AdminTools.IsAdmin(data.ApiKey))
+		{
+			return Json(new GetUserAnswersReturnJson());
+		}
+
+		var user = await _context.UserEntities.Where(x => x.Id == data.UserId).FirstOrDefaultAsync();
+
+		var userAnswers = await _context.UserAnswerEntities.Include(x => x.Question).Where(x => x.User == user).ToListAsync();
+
+		return Json(userAnswers);
 	}
 }
---
+++ quiz_app_api/Data/JsonModels/UserAnswers/Output/GetUserAnswersReturnJson.cs
@@ -0,0 +1,12 @@
+﻿using quiz_app_api.Data.JsonModels.Questions.Output;
+using System.Text.Json.Serialization;
+
+namespace quiz_app_api.Data.JsonModels.UserAnswers.Output;
+
+public class GetUserAnswersReturnJson
+{
+	[JsonPropertyName("question")]
+	public GetAllQuestionsReturnJson Question { get; set; }
+	[JsonPropertyName("chosen_option")]
+	public int ChosenOption { get; set; }
+}
