44dc3d525d48246e518e02def60257d4b074ad7f
Matetorture <przemek.pytlarz18@gmail.com>
2024-04-11 22:18:33 +0200

    Updated button CSS in endScreen

--- QuizApp/src/js/endScreen.js
+++ QuizApp/src/js/endScreen.js
@@ -11,8 +11,10 @@ setInterval(async () => {
             window.location.href = 'login.html';
         } else if (systemStatusData === 2) {
             showLeaderboard.innerHTML = "Poczekaj na zakończenie testu";
+            showLeaderboard.style.backgroundColor = "var(--primary)";
         } else if (systemStatusData === 3) {
             showLeaderboard.innerHTML = "Zobacz tabele wyników";
+            showLeaderboard.style.backgroundColor = "#3b3b3b";
 
             showLeaderboard.addEventListener("click", () => {
                 window.open("./leaderboard.html"); 
