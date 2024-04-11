2ae4932cd6bd6169673af8fda48ea0f6803a727e
Robert Szczotka <98053646+arkis0@users.noreply.github.com>
2024-04-11 20:00:40 +0200

    - login rework to  use new status codes - save user_id in a cookie

--- QuizApp/src/js/login.js
+++ QuizApp/src/js/login.js
@@ -1,79 +1,76 @@
 window.onload = function () {
   document.getElementById("form").onsubmit = function () {
-    return false;
+      return false;
   };
 };
 
 function getCookie(name) {
   let cookieArr = document.cookie.split("; ");
-  for(let i = 0; i < cookieArr.length; i++) {
+  for (let i = 0; i < cookieArr.length; i++) {
       let cookiePair = cookieArr[i].split("=");
-      if(name == cookiePair[0]) {
+      if (name == cookiePair[0]) {
           return decodeURIComponent(cookiePair[1]);
       }
   }
   return null;
 }
+
 let api_key = getCookie('api_key');
 if (api_key !== null) {
   window.location.href = 'info.html';
 }
 
-
 function sendApiRequest() {
-  fetch('http://localhost:5000/api/systemstatus/GetSystemStatus')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(systemStatusData => {
-      if (systemStatusData === 0) {
-        console.log('Server is down');
-      }
-      else {
-        var login = document.getElementById("login").value;
-        var password = document.getElementById("password").value;
-        if (login != undefined && login != null && login != "" && password != undefined && password != null && password) {
-          const myHeaders = new Headers();
-          myHeaders.append("Content-Type", "application/json");
-          myHeaders.append("Access-Control-Allow-Origin", "*");
-          myHeaders.append("Origin", "*");
-          myHeaders.append("credentials", "include");
+  var login = document.getElementById("login").value;
+  var password = document.getElementById("password").value;
+  if (login && password) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Access-Control-Allow-Origin", "*");
+      myHeaders.append("Origin", "*");
+      myHeaders.append("credentials", "include");
 
-          const data = JSON.stringify({
-            login: login,
-            password: password,
-          });
+      const data = JSON.stringify({
+          login: login,
+          password: password,
+      });
 
-          const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: data,
-            redirect: "follow",
-          };
-          //api url
-          fetch("http://localhost:5000/api/users/Login/", requestOptions)
-            .then((response) => response.text())
-            .then((result) => {
-              var api_key = JSON.parse(result).api_key;
-              if (api_key != undefined) {
-                document.cookie = "api_key=" + api_key + "; path=/;";
-                document.getElementById("messageBox").innerText = "udało się zalogować";
-                window.location.href = "info.html";
+      const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: data,
+          redirect: "follow",
+      };
+
+      fetch(`${config.api_url}/api/users/Login/`, requestOptions)
+          .then((response) => {
+              if (response.status === 200) {
+                  return response.text(); // Return response.text() to the next .then() block
+              } else if (response.status === 403) {
+                  document.getElementById("messageBox").innerText = "Serwer ma status 0";
+                  throw new Error('Server status 0');
+              } else if (response.status === 400) {
+                  document.getElementById("messageBox").innerText = "Nie znaleziono użytkownika o takim loginie i haśle";
+                  throw new Error('User not found');
               } else {
-                document.getElementById("messageBox").innerText = "logowanie nieudane";
+                  throw new Error('Network response was not ok');
               }
-            })
-            .catch((error) => console.error(error));
-        }
-        else {
-          document.getElementById("messageBox").innerText = "uzupełnij wszystkie pola";
-        }
-      }
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
+          })
+          .then((result) => { // Handle result here
+              var api_key = JSON.parse(result).api_key;
+              var user_id = JSON.parse(result).user_id;
+              if (api_key != undefined) {
+                  document.cookie = "api_key=" + api_key + "; path=/;";
+                  document.cookie = "user_id=" + user_id + "; path=/;";
+                  document.getElementById("messageBox").innerText = "udało się zalogować";
+                  window.location.href = "info.html";
+              } else {
+                  document.getElementById("messageBox").innerText = "logowanie nieudane";
+              }
+          })
+          .catch((error) => console.error(error));
+  }
+  else {
+      document.getElementById("messageBox").innerText = "uzupełnij wszystkie pola";
+  }
 }
