98b73bdc8456c1b6f677e4c5203e412437ed3102
Matetorture <przemek.pytlarz18@gmail.com>
2024-04-11 22:15:51 +0200

    Added inability to go to the leaderboard if systemStatus is not equal to 3

--- QuizApp/src/js/endScreen.js
+++ QuizApp/src/js/endScreen.js
@@ -1,5 +1,30 @@
 const showLeaderboard = document.querySelector("#show-leaderboard");
-console.log("s")
-showLeaderboard.addEventListener("click", () => {
-    window.open("./leaderboard.html"); 
-});
+
+setInterval(async () => {
+    try {
+        const response = await fetch(`${config.api_url}/api/systemstatus/GetSystemStatus`);
+        const systemStatusData = await response.json();
+
+        if (systemStatusData === 0) {
+            window.location.href = 'login.html';
+        } else if (systemStatusData === 1) {
+            window.location.href = 'login.html';
+        } else if (systemStatusData === 2) {
+            showLeaderboard.innerHTML = "Poczekaj na zakończenie testu";
+        } else if (systemStatusData === 3) {
+            showLeaderboard.innerHTML = "Zobacz tabele wyników";
+
+            showLeaderboard.addEventListener("click", () => {
+                window.open("./leaderboard.html"); 
+            });
+
+        } else {
+            console.log('Unknown status');
+        }
+    } catch (error) {
+        clearInterval();
+        console.error(error);
+        alert('Server failed to respond. Please try again later.')
+        window.location.href = 'login.html';
+    }
+}, 5000);
